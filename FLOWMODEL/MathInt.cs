6b8970095a64c206d0fb888bdb8bc0b5c051c6d9
gusdak <gusdak@gmail.com>
2015-04-04 01:03:51 +0300

    Example of interface

--- FLOWMODEL/MathInt.cs
+++ FLOWMODEL/MathInt.cs
@@ -8,6 +8,28 @@ namespace FLOWMODEL
 {
     interface MathInt
     {
-		public void SomeMath();
-    }
+		// Поправочный коэффициент F
+		double CorrectionCoefficientF(double H, double W);
+		// H -- ширина
+		// W -- длинна
+		
+		// Объемный расход Q
+		double VolumetricFlowRateQ(double H, double W, double Vu, double F);
+		// H -- ширина
+		// W -- длинна
+		// Vu -- скорость верней крышки канала
+
+		// Скорость деформации сдвига
+		double ShearStainRateGamma(double H, double Vu);
+		// H -- ширина
+		// Vu -- скорость верней крышки канала
+		
+		// Удельный тепловой поток
+		double HeatFluxQGamma(double H, double W, double Mu0, double N, double Gamma);
+		// H -- ширина
+		// W -- длинна
+		// Mu0 -- коэффициент консистенции при температуре приведения
+		// N -- индекс течения материала
+		// Gamma -- скорость деформации сдвига
+	}
 }
