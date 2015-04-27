81e9de9bd14071fb4c9581a9849cfb40fa6b5158
cathodeyolk <poothere@gmail.com>
2015-04-27 04:14:58 +0300

    Added database support. Temporarily disabled OxyPlot.

---
+++ FLOWMODEL/Database.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using System.Data.SQLite;
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FLOWMODEL
+{
+	// Структура с описанием материала
+	struct Materials
+	{
+		public int ID { get { return _id; } set { _id = value; } }
+		int _id;
+
+		public string Name { get { return _name; } set { _name = value; } }
+		string _name;
+
+		public string Desc { get { return _desc; } set { _desc = value; } }
+		string _desc;
+
+		public Materials(int _id, string _name, string _desc)
+		{
+			this._id = _id;
+			this._name = _name;
+			this._desc = _desc;
+		}
+	}
+
+	class Database
+	{
+		private SQLiteConnection dbConnection;
+
+		// Превращение класса в singleton-класс с помощью паттерна
+		public static readonly Database _instance = new Database();
+		Database() { }
+
+		// Список структур материалов
+		List<Materials> MaterialsList = new List<Materials>();
+
+		public void Connect()
+		{
+			dbConnection = new SQLiteConnection(@"DataSource=..\..\Database\FlowmodelDB.db3;Version=3;");
+			try
+			{
+				dbConnection.Open();
+			}
+			catch (SQLiteException ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		public void Close()
+		{
+			dbConnection.Close();
+		}
+
+		public void GetMaterials(object TargetCombobox)
+		{
+			ComboBox cb = (ComboBox)TargetCombobox;
+			cb.Items.Clear();
+			MaterialsList.Clear();
+
+			SQLiteCommand command = new SQLiteCommand("SELECT * FROM Materials", dbConnection);
+			SQLiteDataReader reader = command.ExecuteReader();
+
+			while (reader.Read())
+			{
+				Console.WriteLine(reader["ID"] + " " + reader["Name"] + " " + reader["Description"]);
+				MaterialsList.Add(new Materials(Convert.ToInt32(reader["ID"]), (string)reader["Name"], (string)reader["Description"]));
+			}
+
+			cb.ItemsSource = MaterialsList;
+			cb.DisplayMemberPath = "Name";
+			cb.SelectedValuePath = "ID";
+			cb.SelectedIndex = 0;
+		}
+
+		public SQLiteDataReader GetMaterialSettings(int materialID)
+		{
+			SQLiteCommand command = new SQLiteCommand("SELECT * FROM [Parameters] WHERE [Parameters].[MaterialID] = " + materialID, dbConnection);
+			return command.ExecuteReader();
+		}
+	}
+}
--- FLOWMODEL/MainWindow.xaml.cs
+++ FLOWMODEL/MainWindow.xaml.cs
@@ -15,19 +15,24 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
 using OxyPlot;
+using System.Data;
+using System.Data.SQLite;
 
 namespace FLOWMODEL
 {
 	public partial class MainWindow : Window
 	{
-		public string Title { get; private set; }
-		public IList<DataPoint> TemperaturePoints { get; set;}
+		public IList<DataPoint> TemperaturePoints { get; set; }
 		public IList<DataPoint> ViscosityPoints { get; set; }
 
         private MathModel DefaultModel;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			Database._instance.Connect();
+			Database._instance.GetMaterials(MaterialTypeCombox);
 		}
 
 		// Вывод диалогового окна с подтверждением при закрытии программы
@@ -44,6 +49,9 @@ namespace FLOWMODEL
 			}
 		}
 
+		// ----------------------------------------------------------------------------------------------------------
+		// РАБОТА С ВКЛАДКАМИ
+		// ----------------------------------------------------------------------------------------------------------
 		// Переключение видимости WPF Grid'ов при выборе вкладки "Расчет"
 		// Grid расчетов - показан, Grid результатов - скрыт, Grid графиков - скрыт
 		private void CalculationsButton_Click(object sender, RoutedEventArgs e)
@@ -66,14 +74,18 @@ namespace FLOWMODEL
 		// Grid расчетов - скрыт, Grid результатов - скрыт, Grid графиков - показан
 		private void GraphButton_Click(object sender, RoutedEventArgs e)
 		{
-			var plotModel1 = new PlotModel();
 			CalcGrid.Visibility = Visibility.Hidden;
 			ResultGrid.Visibility = Visibility.Hidden;
 			GraphGrid.Visibility = Visibility.Visible;
-			ViscosityLine.ItemsSource = ViscosityPoints;
-			TemperatureLine.ItemsSource = TemperaturePoints;
+
+			var plotModel1 = new PlotModel();
+			//ViscosityLine.ItemsSource = ViscosityPoints;
+			//TemperatureLine.ItemsSource = TemperaturePoints;
 		}
 
+		// ----------------------------------------------------------------------------------------------------------
+		// КНОПКА "РАССЧИТАТЬ"
+		// ----------------------------------------------------------------------------------------------------------
 		// Нажата кнопка РАССЧИТАТЬ
         private void CalculateButton_Click(object sender, RoutedEventArgs e)
         {
@@ -85,52 +97,57 @@ namespace FLOWMODEL
 			DefaultModel = new MathModel(H_TBox.Text, W_TBox.Text, Vu_TBox.Text,Mu0_TBox.Text, N_TBox.Text, L_TBox.Text,
 				DeltaL_TBox.Text, B_TBox.Text, Tr_TBox.Text, Tu_TBox.Text, Alpha_TBox.Text, Ro_TBox.Text, C_TBox.Text, Tm_TBox.Text);
 
-			// Если исходная строка ошибки изменялась (к ней прибавлялись
-			// перечисления ошибочных параметров)
+			// Если исходная строка ошибки изменялась (к ней прибавлялись перечисления ошибочных параметров)
 			if (!DefaultModel.GetError().Equals("Некорректные значения: "))
 			{
 				MessageBox.Show(DefaultModel.GetError(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
 			}
-			// В ином случае (строка ошибки не менялась от исходной) мы запускает алгоритм
+			// В ином случае (строка ошибки не менялась от исходной) мы запускаем алгоритм
 			else
 			{
 				try
 				{
+					// Инициализация таймера
 					AlgoritmTime = new Stopwatch();
 					AlgoritmTime.Start();
 					DefaultModel.Algorithm();
 					AlgoritmTime.Stop();
 					Time = Convert.ToString(AlgoritmTime.ElapsedMilliseconds);
-
-					Tp_TBox.Text = Convert.ToString(DefaultModel.GetTp(), CultureInfo.InvariantCulture);
-					EtaP_TBox.Text = Convert.ToString(DefaultModel.GetEtaP(), CultureInfo.InvariantCulture);
-					G_TBox.Text = Convert.ToString(DefaultModel.GetG(), CultureInfo.InvariantCulture);
  
-					// Получение массивов температур и вязкостей для таблицы
+					// Получение массивов температур и вязкостей для таблицы и графиков
 					T = DefaultModel.GetTI();
 					Eta = DefaultModel.GetEtaI();
 
-					List<DataGridItem> list = new List<DataGridItem>();
-					Title = "Title";
+					// Объявление списка данных для заполнения таблицы
+					List<DataGridItem> DataGridItemsList = new List<DataGridItem>();
+					// Объявление списков точек на графике для показателей температуры и вязкости
 					TemperaturePoints = new List<DataPoint>();
 					ViscosityPoints = new List<DataPoint>();
 
 					for (int i = 0; i < T.Length; i++)
 					{
+						// Заполнение таблицы рассчитанными значениями
+						DataGridItemsList.Add(new DataGridItem() { id = Convert.ToDouble(i) / 10, T = T[i], Eta = Eta[i] });
+						// Добавление точек на графики температуры и вязкости (отображаются через биндинг)
 						TemperaturePoints.Add(new DataPoint(Convert.ToDouble(i) / 10, T[i]));
 						ViscosityPoints.Add(new DataPoint(Convert.ToDouble(i) / 10, Eta[i]));
-
-						list.Add(new DataGridItem() { id = Convert.ToDouble(i) / 10, T = T[i], Eta = Eta[i] });
 					}
 
-					ResultsGataGrid.ItemsSource = list;
+					// Вывод рассчитанных значений температуры, вязкости и производительности в текстовые поля
+					Tp_TBox.Text = Convert.ToString(DefaultModel.GetTp(), CultureInfo.InvariantCulture);
+					EtaP_TBox.Text = Convert.ToString(DefaultModel.GetEtaP(), CultureInfo.InvariantCulture);
+					G_TBox.Text = Convert.ToString(DefaultModel.GetG(), CultureInfo.InvariantCulture);
 
+					// Привязка рассчитанных значений к таблице результатов
+					ResultsGataGrid.ItemsSource = DataGridItemsList;
+					// Название столбцов таблицы
 					ResultsGataGrid.Columns[0].Header = "Длина, м";
 					ResultsGataGrid.Columns[1].Header = "Температура, С";
 					ResultsGataGrid.Columns[2].Header = "Вязкость, Па*с";
 
-					MessageBox.Show("Рассчеты успешно произведены. \n Затраченное время: " + Time + " мс", "Готово", MessageBoxButton.OK, MessageBoxImage.Information);
+					MessageBox.Show("Расчеты успешно произведены. \n Затраченное время: " + Time + " мс", "Готово", MessageBoxButton.OK, MessageBoxImage.Information);
 
+					// Переключение на вкладку с результатами
 					CalcGrid.Visibility = Visibility.Hidden;
 					ResultGrid.Visibility = Visibility.Visible;
 				}
@@ -140,8 +157,19 @@ namespace FLOWMODEL
 					DefaultModel = null;
 				}
 			}
-
-		DefaultModel = null;
+			DefaultModel = null;
         }
+
+		private void MaterialTypeCombox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			SQLiteDataReader rd = Database._instance.GetMaterialSettings(Convert.ToInt32(MaterialTypeCombox.SelectedValue));
+			rd.Read();
+			// Плотность
+			Ro_TBox.Text = rd["Density"].ToString();
+			// Теплоемкость
+			C_TBox.Text = rd["HeatCapacity"].ToString();
+			// Температура плавления
+			Tm_TBox.Text = rd["TemperatureMelting"].ToString();
+		}
 	}
 }
